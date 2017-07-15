2f74f2fd9064442c26f058947356b3c993fbbea1
nikolai <n.ivanov.p@gmail.com>
2017-07-15 16:02:02 +0300

    compose-test - 1

--- cfcr-speed.sh
+++ cfcr-speed.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-echo Pulling the image from Dockerhub
 echo Start
 date
 date +%s
 
-docker pull garima0079/bigimage
+#docker pull garima0079/bigimage
+#docker push r.cfcr.io/nikolai/garima0079/bigimage
+docker pull r.cfcr.io/nikolai/garima0079/bigimage
 
 date
 date +%s
