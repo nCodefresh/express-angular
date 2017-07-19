4cf3b9f12f2d9db2ce3ea2461435c80cf370d3c6
nikolai <n.ivanov.p@gmail.com>
2017-07-19 17:18:01 +0300

    test-env-vars - 6

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-echo test
-#[ $CF_BRANCH=='develop' ]
+
+if [ "$CF_BRANCH" == "staging" ]
+    then
+	bundle exec bin/f2y-aws-tool deploy --target=file:./deploy/hurricane-api-staging.yml & bundle exec bin/f2y-aws-tool deploy --target=file:./deploy/hurricane-staging.yml
+elif [ "$CF_BRANCH" == "master" ]
+    then
+    bundle exec bin/f2y-aws-tool deploy --target=file:./deploy/hurricane-api-production.yml & bundle exec bin/f2y-aws-tool deploy --target=file:./deploy/hurricane-production.yml
+else
+    echo "Deployment disabled for branch $CF_BRANCH"
+    exit 0
+fi
